11fff9ab2d2f8420a7edb80451265c9dcf98d813
Ivan Grachev <grachev.ivan@hotmail.com>
2022-03-29 04:22:01 +0300

    add appolo connection and sample query for messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./Header/Header";
+import ChatContainer from "./ChatContainer/ChatContainer";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://angular-test-backend-yc4c5cvnnq-an.a.run.app/graphql',
+  cache: new InMemoryCache()
+});
+
+
+client
+.query({
+  query: gql`
+    query {
+      fetchLatestMessages(channelId: "1"){
+        messageId
+        text
+        datetime
+        userId
+      }
+    }
+  `
+})
+.then(result => console.log(result));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ApolloProvider client={client} >
+        <Header />
+        <ChatContainer />
+      </ApolloProvider>
+    </>
   );
 }
 
